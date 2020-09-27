081df67869c995103318563e6ed234438a29cd03
mochorak <mohammad.chorakchi@akelius.de>
2020-09-27 21:47:39 +0200

    Iplement page component

---
+++ src/pages/HomePage/HomePage.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { Questions } from "./../../modules/Questions/Questions.component";
+
+export const HomePage = () => {
+  return (
+    <div>
+      <Questions />
+    </div>
+  );
+};
